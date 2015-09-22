42a913766deb03fcf652a84c940a3c5ed1cae1a3
Asher Anjum <asher_as@live.com>
2015-09-22 16:56:05 +0500

    changed redirect uri

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var fbg = require('fbgraph');
 
 var conf = {
 	client_id: process.env.FB_CLIENT_ID,
-	redirect_uri: 'http://gop-dev.elasticbeanstalk.com/',
+	redirect_uri: 'http://gopapp.heroku.com/',
 	port: process.env.PORT || 8888,
 	permissions: ['public_profile', 'email', 'user_friends']
 }
